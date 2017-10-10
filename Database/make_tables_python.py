50a6913987193b5f9773f71889761464e694d8f1
apmechev <apmechev@gmail.com>
2017-10-10 14:55:54 +0200

    Added a test case for the make_tables_python, test with 'nosetests'

--- Database/make_tables_python.py
+++ Database/make_tables_python.py
@@ -11,62 +11,72 @@ from astropy.table import Table
 # First read in the datafiles. I use astropy Table because
 # this can be easily modified if we were to change the format
 # in the future.
-stars = Table().read('YAEPS.stars-table.dat',  format='csv')
-observations = Table().read('YAEPS.observations-table.dat',  format='csv')
 
+def load_data(filename=None):
+    """Loads the data from a text file into an Astropy Table
+    and returns the table. Throws if no file
+    """
+    if filename: return Table().read(filename,  format='csv')
+    else: raise IOError("No input file given")
 
 # Next, we create a connection to the database.
-con = lite.connect("DDM17-python.db")
 
-with con:
-
-
-    # Create the command to create the table. I use a 
-    # multiline string to ease readability here.
-    table = 'Stars'
-    command = """CREATE TABLE IF NOT EXISTS {0} (StarID INT,
-			 FieldID INT, Star varchar(10), ra DOUBLE,
-			 decl DOUBLE, g FLOAT, r FLOAT,
-			 UNIQUE(StarID), PRIMARY KEY(StarID),
-			 FOREIGN KEY(FieldID) REFERENCES Observations(ID))""".format(table)
-
-    # Next, actually execute this command.
-    con.execute(command)
-
-    # Now that this is working, let us loop over the table entries
-    # and insert these into the table.
-    for row in stars:
-        command = "INSERT INTO Stars VALUES({0},{1},'{2}',{3},{4},{5},{6})".format(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
-        print command
+def load_db(dbname="DDM17-python.db"):
+    """Loads the Stars and Observation tables into the appropriate database. 
+    """
+    con = lite.connect(dbname)
+    
+    with con:
+    
+    
+        # Create the command to create the table. I use a 
+        # multiline string to ease readability here.
+        table = 'Stars'
+        command = """CREATE TABLE IF NOT EXISTS {0} (StarID INT,
+    			 FieldID INT, Star varchar(10), ra DOUBLE,
+    			 decl DOUBLE, g FLOAT, r FLOAT,
+    			 UNIQUE(StarID), PRIMARY KEY(StarID),
+    			 FOREIGN KEY(FieldID) REFERENCES Observations(ID))""".format(table)
+    
+        # Next, actually execute this command.
         con.execute(command)
-
-
-
-
-    #
-    # Create the observations table.
-    #
-    table = 'Observations'
-    command = """
-CREATE TABLE IF NOT EXISTS Observations (ID INT,
-     	    Field varchar(10),
-  		    date DOUBLE, 
-		    exptime FLOAT,
-		    quality FLOAT, 
-  		    WhereStored varchar(256),
-		    UNIQUE (ID),
-		    PRIMARY KEY (ID)
-			);
-""".format(table)
-
-    # Next, actually execute this command.
-    con.execute(command)
-
-    # Now that this is working, let us loop over the table entries
-    # and insert these into the table.
-    #
-    # Note the '' around the string values - try to remove them, it won't go well!
-    for row in observations:
-        command = "INSERT INTO Observations VALUES({0},'{1}',{2},{3},{4},'{5}')".format(row[0], row[1], row[2], row[3], row[4], row[5])
-        print command
+    
+        # Now that this is working, let us loop over the table entries
+        # and insert these into the table.
+        stars=load_data('YAEPS.stars-table.dat')
+        for row in stars:
+            command = "INSERT INTO Stars VALUES({0},{1},'{2}',{3},{4},{5},{6})".format(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+            print command
+            con.execute(command)
+   
+        #
+        # Create the observations table.
+        #
+        table = 'Observations'
+        command = """
+    CREATE TABLE IF NOT EXISTS Observations (ID INT,
+         	    Field varchar(10),
+      		    date DOUBLE, 
+    		    exptime FLOAT,
+    		    quality FLOAT, 
+      		    WhereStored varchar(256),
+    		    UNIQUE (ID),
+    		    PRIMARY KEY (ID)
+    			);
+    """.format(table)
+    
+        # Next, actually execute this command.
         con.execute(command)
+    
+        # Now that this is working, let us loop over the table entries
+        # and insert these into the table.
+        #
+        # Note the '' around the string values - try to remove them, it won't go well!
+        observations=load_data('YAEPS.observations-table.dat')
+        for row in observations:
+            command = "INSERT INTO Observations VALUES({0},'{1}',{2},{3},{4},'{5}')".format(row[0], row[1], row[2], row[3], row[4], row[5])
+            print command
+            con.execute(command)
+
+if __name__ == "__main__":
+    load_db(dbname="DDM17-python.db")
---
+++ tests/test_make_tables.py
@@ -0,0 +1,37 @@
+import unittest
+from Database.make_tables_python import *
+import sqlite3 as lite
+import os
+
+class make_tables_test(unittest.TestCase):
+    def setUp(self):
+        self.cwd=os.getcwd()
+
+    def tearDown(self):
+        os.chdir(self.cwd)
+
+    def test_read_table(self):
+        load_data(self.cwd+'/Database/YAEPS.observations-table.dat') 
+        load_data(self.cwd+'/Database/YAEPS.stars-table.dat')
+
+    def test_load_raises(self):
+        self.assertRaises(IOError, load_data)
+
+    def test_load_db(self):
+        os.chdir(self.cwd+'/Database')
+        load_db(dbname="nose_test.db") 
+        con=lite.connect('nose_test.db')
+        with con:
+            command="SELECT name FROM sqlite_master WHERE type='table';"
+            x=con.execute(command)
+        o=[]
+        for i in x:
+            o.append(str(i[0]))
+        self.assertTrue(len(o)==2)
+        self.assertTrue('Stars' in o)
+        self.assertTrue('Observations' in o)
+        os.remove('nose_test.db')
+        os.chdir(self.cwd)
+
+
+
